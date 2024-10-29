30131e4bf0e7fb8ec11b670bab7a46e46afd3c4e
najd-04 <najdouifak@gmail.com>
2024-10-29 16:57:38 +0100

    ajout des commentaire et des roles

---
+++ assets/sql/wish.sql
@@ -0,0 +1,7 @@
+
+INSERT INTO category (name) VALUES
+                                ('Travel & Adventure'),
+                                ('Sport'),
+                                ('Entertainment'),
+                                ('Human Relations'),
+                                ('Other');
